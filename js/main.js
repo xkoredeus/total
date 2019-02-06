7df2b3480017e6e70c6af17f5436491362a6b56c
xkored <xkored@gmail.com>
2019-02-06 15:27:54 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.main-slider').owlCarousel({
+      nav: false,
+      dots: true,
+      items: 1,
+      loop: false,
+      // navText: ["<img src='img/slider__prev.svg'>", "<img src='img/slider__next.svg'>"]
+});
